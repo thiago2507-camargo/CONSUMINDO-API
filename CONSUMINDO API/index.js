204c2ceb81904ad72db2870568d7709fc60de5ab
Thiago Camargo <thiagobuchard25@gmail.com>
2025-02-03 17:25:47 -0400

    Add files via upload

---
+++ CONSUMINDO API/index.js
@@ -0,0 +1,28 @@
+function Procurar() {
+let País = document.getElementById('País').value
+let finalURL= `https://restcountries.com/v3.1/name/${País}?fullText=true`
+console.log(finalURL)
+fetch(finalURL)
+.then(function(response){
+return response.json()
+})
+.then(function(data) {
+    console.log(data[0])
+    console.log(data[0].population)
+    console.log(data[0].capital[0])
+    console.log(data[0].continents[0])
+
+    let bandeira = document.getElementById('bandeira')
+    let nome = document.getElementById('nome')
+    let capital = document.getElementById('capital')
+    let pop = document.getElementById('pop')
+    let cont = document.getElementById('cont')
+
+    nome.innerHTML = País
+    capital.innerHTML = data[0].capital[0]
+    cont.innerHTML = data[0].continents[0]
+    pop.innerHTML = data[0].population
+    bandeira.src = data[0].flags.svg
+})
+
+}
